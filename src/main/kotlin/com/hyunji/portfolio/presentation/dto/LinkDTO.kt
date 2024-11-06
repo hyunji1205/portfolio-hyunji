90a79f536723f0dea21469b1cc7d15fee9f08558
hyunji1205 <sguswl1205@gmail.com>
2024-11-07 03:38:46 +0900

    DTO 개발

--- src/main/kotlin/com/hyunji/portfolio/presentation/dto/LinkDTO.kt
+++ src/main/kotlin/com/hyunji/portfolio/presentation/dto/LinkDTO.kt
@@ -1,3 +1,14 @@
 package com.hyunji.portfolio.presentation.dto
 
-class LinkDTO
+import com.hyunji.portfolio.domain.entity.Link
+
+class LinkDTO(
+    val name: String,
+    val content: String
+) {
+    constructor(link: Link) : this(
+        name = link.name.lowercase(),
+        content = link.content
+    )
+
+}
--- src/main/kotlin/com/hyunji/portfolio/presentation/dto/SkillDTO.kt
+++ src/main/kotlin/com/hyunji/portfolio/presentation/dto/SkillDTO.kt
@@ -1,3 +1,14 @@
 package com.hyunji.portfolio.presentation.dto
 
-class SkillDTO
+import com.hyunji.portfolio.domain.entity.Skill
+
+data class SkillDTO(
+    val name: String,
+    val type: String
+) {
+    constructor(skill: Skill) : this(
+        name = skill.name,
+        type = skill.type.name
+    )
+
+}
